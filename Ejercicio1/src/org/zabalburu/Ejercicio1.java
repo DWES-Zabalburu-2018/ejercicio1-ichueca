9b6c867d18a58f09a8d5cc70b55c5c78e66769c5
Inigo <inigo.chueca@zabalburu.org>
2018-06-27 08:34:53 +0200

    Creado el Primer Servlet de Prueba con Bootstrap

---
+++ Ejercicio1/src/org/zabalburu/Ejercicio1.java
@@ -0,0 +1,62 @@
+package org.zabalburu;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Ejercicio1
+ */
+@WebServlet("/Ejercicio1")
+public class Ejercicio1 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Ejercicio1() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		PrintWriter out = response.getWriter();
+		response.setContentType("text/html");
+		out.println("<!doctype html>");
+		out.println("<html lang='es'>");
+		out.println("<head>");
+		out.println("<meta charset='utf-8'>"+
+		    "\n<meta name='viewport' content='width=device-width, initial-scale=1, shrink-to-fit=no'>"+
+		    "\n<!-- Bootstrap CSS -->" +
+		    "\n<link rel='stylesheet' href='https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css' integrity='sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB' crossorigin='anonymous'>" +
+		    "\n<title>Hello, world!</title>" +
+		    "\n</head>" +
+		    "\n<body>");
+		out.println("\t<h1>Hello, world!</h1>");
+		out.println("\n\n<script src='https://code.jquery.com/jquery-3.3.1.slim.min.js' integrity='sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo' crossorigin='anonymous'></script>" +
+					"<script src='https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js' integrity='sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49' crossorigin='anonymus'></script>" +
+					"<script src='https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js' integrity='sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T' crossorigin='anonymous'></script>" +
+					"\n</body>" +
+					"\n</html>");
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
